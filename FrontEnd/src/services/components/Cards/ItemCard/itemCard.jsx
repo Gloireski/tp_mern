d0a88452a053d975cc2ccd3c43d2cc03502eda20
NihachiOokami <paulsode05@gmail.com>
2025-02-20 12:53:36 +0100

    removed useless informations in main page

--- FrontEnd/src/services/components/Cards/ItemCard/itemCard.jsx
+++ FrontEnd/src/services/components/Cards/ItemCard/itemCard.jsx
@@ -1,12 +1,10 @@
 ﻿import style from "./itemCard.module.css";
 
-const ItemCard = ({ name, description, price, image_url }) => {
+const ItemCard = ({ name, image_url }) => {
     return (
         <div className={style.card}>
             <img src={image_url} alt={name} />
             <h1>{name}</h1>
-            <p>{price} €</p>
-            <p>{description}</p>
         </div>
     );
 };
